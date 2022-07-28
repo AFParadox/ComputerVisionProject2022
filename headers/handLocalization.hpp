2e26f4643bc263fe5b2ec0659bd3c534f3a8f943
maxb998 <102738073+maxb998@users.noreply.github.com>
2022-07-28 17:01:22 +0200

    Update handLocalization.hpp
    Added descriptive comments

--- headers/handLocalization.hpp
+++ headers/handLocalization.hpp
@@ -5,10 +5,10 @@
 #include <opencv2/dnn.hpp>
 
 
-cv::dnn::Net loadModel(std::string modelPath);
-std::vector<cv::Rect> localizeHands(cv::Mat &img, cv::dnn::Net &net);
-cv::Mat letterbox(cv::Mat &img, cv::Size new_shape, cv::Scalar color, bool _auto, bool scaleFill, bool scaleup, int stride);
-void convertLetterboxCoords(cv::Rect &bbox, cv::Mat &img);
+cv::dnn::Net loadModel(std::string modelPath);  // load network model(avoid loading it each time one need to process an image)
+std::vector<cv::Rect> localizeHands(cv::Mat &img, cv::dnn::Net &net);   // Run yolov5 model on one image and returns a vector of rect representing the bounding boxes(Copied and pasted from github and the adapted to our needs)
+cv::Mat letterbox(cv::Mat &img, cv::Size new_shape, cv::Scalar color, bool _auto, bool scaleFill, bool scaleup, int stride);    // function needed to resize the image in orther to be in line with yolov5 standard(FUNCTION COPIED AND PASTED FROM GITHUB)
+void convertLetterboxCoords(cv::Rect &bbox, cv::Mat &img);  // yolo-scaled image has different bounding box coordinates compared to the original one, so there is the need to conver them
 
 double computeIOU(std::vector<cv::Rect> trueBBoxes, std::vector<cv::Rect> bboxes, int rows, int cols);
 
